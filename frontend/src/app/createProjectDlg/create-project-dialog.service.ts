4d03e8cab8346e76e3b17a8fb0eeca1c091f85a6
Hassen <esmaelmh@hi.no>
2019-10-03 09:39:13 +0200

    code cleaned up

--- frontend/src/app/createProjectDlg/create-project-dialog.service.ts
+++ frontend/src/app/createProjectDlg/create-project-dialog.service.ts
@@ -7,37 +7,11 @@ import { DataService } from '../data/data.service';
 })
 export class CreateProjectDialogService {
 
-  constructor(private dataService: DataService) { }
+  constructor() {}
 
   display: boolean = false;
-  // projectRootPath: string;
-  // project: string;
-  // templates: Template[];
 
   async showDialog() {
-      // console.log("showDialog");
-      // this.projectRootPath = <string>await this.dataService.getProjectRootPath().toPromise();
-      // console.log("project root path retrieved: " + this.projectRootPath);
-      // this.project = <string>await this.dataService.getProjectPath().toPromise();
-      // console.log("project path retrieved: " + this.project);
-      // this.templates = <Template[]> JSON.parse( await this.dataService.getAvailableTemplates().toPromise());
-      // console.log("templates retrieved: " + this.templates.length);
       this.display = true;
-  }  
-
-  // async browse() {
-  //   console.log("browse");
-  //   this.projectRootPath = await this.dataService.browse(this.projectRootPath).toPromise();
-  // } 
-  
-  async apply() {
-
-    this.display = false;
-  }  
-
-  // async initData() {
-  //   console.log("initData");
-  //   this.templates = <Template[]> await this.dataService.getAvailableTemplates().toPromise();
-    
-  // } 
+  }
 }
