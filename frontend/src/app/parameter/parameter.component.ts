5189f0083fa35303e4db8822f4adf152e073a5af
Hassen <esmaelmh@hi.no>
2019-11-13 10:50:10 +0100

    code updated

--- frontend/src/app/parameter/parameter.component.ts
+++ frontend/src/app/parameter/parameter.component.ts
@@ -4,6 +4,8 @@ import { ProjectService } from '../service/project.service';
 import { PropertyItem } from '../data/propertyitem';
 import { PropertyCategory } from '../data/propertycategory';
 import { DataService } from '../service/data.service';
+import { MessageService } from '../message/MessageService';
+// import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-parameter',
@@ -17,34 +19,9 @@ export class ParameterComponent implements OnInit {
     { field: 'value', header: 'Value', width: '50%' }
   ];
 
-  // rowData = [{
-  //   category: 'Process',
-  //   parameters: [
-  //     { name: 'Process name', value: 'ReadBioticXML', type: 'string' },
-  //     { name: 'Function', value: 'ReadBioticXML', type: 'string' },
-  //     { name: 'Enabled', value: 'false', type: 'boolean' }
-  //   ]
-  // },
-  // {
-  //   category: 'GUI',
-  //   parameters: [
-  //     { name: 'Break in GUI', value: 'true', type: 'boolean' },
-  //     { name: 'Respond in GUI', value: 'true', type: 'boolean' }
-  //   ]
-  // },
-  // {
-  //   category: 'Parameters',
-  //   parameters: [
-  //     { name: 'BioticData', value: 'ReadBioticXML', type: 'string' },
-  //     { name: 'FishStationExpr', value: "fs.getLengthSampleCount('TORSK')", type: 'string' },
-  //     { name: 'CatchExpr', value: "species == '164712'", type: 'string' }
-  //   ]
-  // }
-  // ];
 
-
-  //  booleanForm: FormGroup;
-  constructor(public ps: ProjectService, private dataService: DataService) { }
+  //  booleanForm: FormGroup; private msgService: MessageService,
+  constructor(private msgService: MessageService, public ps: ProjectService, private dataService: DataService) { }
 
   async ngOnInit() {
 /*    let a = [];
@@ -65,8 +42,17 @@ export class ParameterComponent implements OnInit {
 
     // groupName: string, name: string, value: string, projectPath: string, modelName: string, processID: string
     if(this.ps.getSelectedProject() != null && this.ps.getSelectedProcess() != null && this.ps.getSelectedModel() != null) {
-        this.ps.propertyCategories = <PropertyCategory[]>JSON.parse( await this.dataService.setProcessPropertyValue(category.groupName, pi.name, pi.value, this.ps.getSelectedProject().projectPath, this.ps.getSelectedModel().modelName, this.ps.getSelectedProcess().processID).toPromise());
-        // await this.dataService.setProcessPropertyValue(category.groupName, pi.name, pi.value, this.ps.getSelectedProject().projectPath, this.ps.getSelectedModel().modelName, this.ps.getSelectedProcess().processID).toPromise();
+       try {
+          this.ps.propertyCategories = <PropertyCategory[]>JSON.parse( await this.dataService.setProcessPropertyValue(category.groupName, pi.name, pi.value, this.ps.getSelectedProject().projectPath, this.ps.getSelectedModel().modelName, this.ps.getSelectedProcess().processID).toPromise());
+          // await this.dataService.set ProcessPropertyValue(category.groupName, pi.name, pi.value, this.ps.getSelectedProject().projectPath, this.ps.getSelectedModel().modelName, this.ps.getSelectedProcess().processID).toPromise();
+       } catch(error) {
+          console.log(error.error);
+          var firstLine = error.error.split('\n', 1)[0];
+          this.msgService.setMessage(firstLine);
+          this.msgService.showMessage();    
+          return;
+       }
+     
     }
   }
 
