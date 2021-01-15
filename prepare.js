3297cad92e0244bdd5651a5cff1b930021fd4197
Skålevik <aasmunds@imr.no>
2021-01-15 10:03:07 +0100

    2.9.19

--- prepare.js
+++ prepare.js
@@ -4,5 +4,5 @@ function copyUrl(url, target) {
     });
 }
 // copy files from github that is needed runtime offline as resources
-copyUrl("https://raw.githubusercontent.com/StoXProject/versions/master/OfficialRstoxFrameworkVersions.txt", "srv/OfficialRstoxFrameworkVersions.txt")
+copyUrl("https://raw.githubusercontent.com/StoXProject/RstoxFramework/develop/inst/versions/OfficialRstoxFrameworkVersions.txt", "srv/OfficialRstoxFrameworkVersions.txt")
 copyUrl("https://raw.githubusercontent.com/StoXProject/RstoxFramework/develop/R/Versions.R", "srv/Versions.R")
