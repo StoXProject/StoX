dc942e03a600aba3d959ee63ef0b8145b4f65bf9
Ibrahim Umar <ibrahim.umar@hi.no>
2021-03-18 13:51:27 +0100

    Use WIX MSI for building installer

---
+++ installers/windows/createmsiinstaller.js
@@ -0,0 +1,28 @@
+const ewm = require('electron-wix-msi');
+const path = require('path')
+
+const rootPath = path.join(process.cwd())
+const outPath = path.join(rootPath, 'dist')
+
+const start = async function() {
+  const msiCreator = new ewm.MSICreator({
+    appDirectory: path.join(outPath, 'StoX-win32-x64'),
+    description: 'StoX: An open source software for marine survey analyses',
+    exe: 'StoX',
+    name: 'StoX',
+    arch: 'x64',
+    manufacturer: 'Institut of Marine Research, Norway',
+    version: '1.0.0',
+    outputDirectory: path.join(outPath, 'installers'),
+    appIconPath: path.join(rootPath, 'assets', 'stox_icon.ico')
+  });
+  const supportBinaries = await msiCreator.create();
+
+  //supportBinaries.forEach(async (binary) => {
+  //  await signFile(binary);
+  //});
+
+  await msiCreator.compile();
+}
+
+start();
