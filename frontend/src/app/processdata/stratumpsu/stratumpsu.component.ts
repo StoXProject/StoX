693bf0752454e33ca03926e3254ef00a83a3b9c8
Åsmund Skålevik <aasmunds@hi.no>
2022-10-04 12:36:56 +0200

    psustratum contextmenu fix, prepare for stop icon

--- frontend/src/app/processdata/stratumpsu/stratumpsu.component.ts
+++ frontend/src/app/processdata/stratumpsu/stratumpsu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TreeNode, MenuItem } from 'primeng/api';
 import { ContextMenu } from 'primeng/contextmenu';
 import { ProcessDataService } from './../../service/processdata.service'
@@ -12,8 +12,7 @@ import { PSUResult, ActiveProcessResult } from './../../data/runresult'
   styleUrls: ['./stratumpsu.component.scss']
 })
 export class StratumpsuComponent implements OnInit {
-  contextMenu: MenuItem[];
-  nodes: TreeNode[];
+  @Input() cm: ContextMenu;  nodes: TreeNode[];
   m_selectedNode: TreeNode;
 
   private static asNode(id: string, type: string, children: TreeNode[]): TreeNode {
@@ -136,19 +135,19 @@ export class StratumpsuComponent implements OnInit {
         break;
       }
     }
-    this.contextMenu = m;
+    this.cm.model = m;
   }
 
-  async openCm(event, cm: ContextMenu, node: TreeNode) {
+  async openCm(event, node: TreeNode) {
     this.selectedNode = node;
     //console.log("selecting process " + process.processID + " in contextmenu handler");
     event.preventDefault();
     event.stopPropagation();
     await this.prepCm(node);
-    if (this.contextMenu.length > 0) {
-      cm.show(event);
+    if (this.cm.model.length > 0) {
+      this.cm.show(event);
     } else {
-      cm.hide();
+      this.cm.hide();
     }
     return false;
   }
