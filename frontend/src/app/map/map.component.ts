49486f04678088f7c047c0377f0fbe708e66eccf
Åsmund Skålevik <aasmund.skaalevik@hi.no>
2022-12-06 20:51:34 +0100

    463

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -450,19 +450,26 @@ export class MapComponent implements OnInit, AfterViewInit, MapInteraction {
         }
         case "selectedStratum": {
           // Remove the check for iamode to update selected when always changed
-//          switch (this.ps.iaMode) {
-//            case "acousticPSU": {
-              this.map.getLayers().getArray()
-                .filter(l => l.get("layerType") == "stratum")
-                .map(l => <VectorSource>(<Layer>l).getSource())
-                .forEach(s => s.getFeatures()
-                  .forEach(f => {
-                    // selected PSU.
-                    MapSetup.updateStratumSelection(f, this.pds.selectedStratum);
-                  }))
-//              break;
-//            }
-//          }
+          let handleStratumSelection : Boolean = false;
+          switch (this.ps.iaMode) {
+            case "acousticPSU": 
+            case "bioticAssignment": {
+              handleStratumSelection = true;
+              break;
+            }
+            default:
+              handleStratumSelection = this.pds.selectedStratum == null;
+          }
+          if(handleStratumSelection) {
+            this.map.getLayers().getArray()
+            .filter(l => l.get("layerType") == "stratum")
+            .map(l => <VectorSource>(<Layer>l).getSource())
+            .forEach(s => s.getFeatures()
+              .forEach(f => {
+                // selected PSU.
+                MapSetup.updateStratumSelection(f, this.pds.selectedStratum);
+              }))
+          }
           break; 
         }
       }
