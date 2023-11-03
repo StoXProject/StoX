92f76f4638c4a3a431494513a86f870d3d2c0300
Ståle Nestås <staale.nestaas@hi.no>
2023-11-03 08:05:48 +0100

    STOX-689: Karma tests

--- frontend/src/app/file-upload/file-upload.component.spec.ts
+++ frontend/src/app/file-upload/file-upload.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FileUploadComponent } from './file-upload.component';
 
@@ -7,7 +7,7 @@ describe('FileUploadComponent', () => {
 
   let fixture: ComponentFixture<FileUploadComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [FileUploadComponent],
     }).compileComponents();
--- frontend/src/app/querybuilder/module/query-builder.component.spec.ts
+++ frontend/src/app/querybuilder/module/query-builder.component.spec.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
 import { QueryBuilderComponent } from './query-builder.component';
@@ -9,7 +9,7 @@ describe('QueryBuilderComponent', () => {
 
   let fixture: ComponentFixture<QueryBuilderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [CommonModule, FormsModule],
       declarations: [QueryBuilderComponent],
