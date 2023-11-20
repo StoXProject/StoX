e558a6fed1ec0065c50f62a4d156608f06e3d645
Ståle Nestås <staale.nestaas@hi.no>
2023-11-20 10:25:34 +0100

    STOX-736: Refactoring

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -398,7 +398,7 @@ export class MapComponent implements OnInit, MapInteraction {
     this.coastLine = new Vector({
       source: new Source({
         //url: 'assets/landflate_verden_gap180.json', //original
-        url: 'assets/toplogy_world_fine.json', 
+        url: 'assets/topology_world_fine.json', 
         //url: 'assets/topology_only_europe_fine.json',
         format: new TopoJSON({
           // don't want to render the full world polygon (stored as 'land' layer),
